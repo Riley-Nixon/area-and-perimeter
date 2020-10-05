6331bb23b8359a39954607952be0a0d6b06ba20e
Riley-Nixon <71408706+Riley-Nixon@users.noreply.github.com>
2020-10-05 14:41:16 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,8 @@ let top = game.askForNumber("enter the top length")
 let bottom = game.askForNumber("enter the base length")
 let slope = game.askForNumber("enter the slope length")
 let height = game.askForNumber("enter the height")
-let area = (bottom + top) * (2 + height)
+let tbx2 = (bottom + top) * 2
+let area = tbx2 + height
 let perimeter = slope * 2 + (top + bottom)
 game.splash("The area is " + convertToText(area) + " cm2")
 game.splash("The perimeter is " + convertToText(perimeter) + " cm")
