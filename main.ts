f83584408114383ad5d5867a042aa1eb86d36a21
Riley-Nixon <71408706+Riley-Nixon@users.noreply.github.com>
2020-10-05 14:11:24 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
-game.splash("Calculating the area and perimeter of a trapazoid")
+// 
+game.splash("Calculating the area and perimeter of a trapezoid")
 let top = game.askForNumber("enter the top length")
-let bottom = game.askForNumber("enter the bottom length")
+let bottom = game.askForNumber("enter the base length")
 let slope = game.askForNumber("enter the slope length")
 let height = game.askForNumber("enter the height")
-let area = height * top
+let area = (bottom + top) * (2 + height)
 let perimeter = slope * 2 + (top + bottom)
 game.splash("The area is " + convertToText(area) + " cm2")
 game.splash("The perimeter is " + convertToText(perimeter) + " cm")
